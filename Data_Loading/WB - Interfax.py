844a60b0e436cc83dd10548b58bd9440f434a0db
Журавлев <pzhuravlev@rccf.ru>
2023-09-07 09:46:52 +0300

    added parsers and db manager

--- Data_Loading/WB - Interfax.py
+++ Data_Loading/WB - Interfax.py
@@ -1,7 +1,8 @@
-from ifxdata import InterfaxData
+import ifxdata
 import importlib
 import sys
-import pyodbc
+import pandas as pd
+# import pyodbc
 # from datetime import datetime as dt
 # from datetime import date
 
@@ -9,129 +10,74 @@ import pyodbc
 InterfaxData = importlib.reload(sys.modules['ifxdata']).InterfaxData
 
 # create object from InterfaxData class
-IData = InterfaxData()
+IData = ifxdata.InterfaxData()
 # pass window login/password for proxy settings
-# IData.set_proxies('login', 'password')
+IData.set_proxies('pzhuravlev', 'RCJobPsw2')
 
 # get token from Interfax
 IData.get_token()
 # free Interfax token
 IData.free_token()
 
-IData.set_dates('2022-01-01', '2022-12-07')
 
-# -------------------------------
-# ------- BOND CONTROLLER -------
-# -------------------------------
 
-# POST Coupons
-IData.set_boards(['TQOB', 'TQCB'])
-IData.set_boards(None)
-IData.set_asset_codes(['SU25084RMFS3', 'RU000A102RN7'])
-# data = IData.get_interfax_data('Bond', 'Coupons', False)
+dct = {'FinToolId': {1, 2}, 'CouponPeriod': {3, 4}, 'PeriodFrom': {5, 6}, 'PeriodTo': {7, 8}, 'PayPerBond': {9, 0}}
+parsedData = pd.DataFrame(dct)
 
 # -------------------------------
 # ------- MOEX Controller -------
 # -------------------------------
 
-# POST Securities
-# IData.SetAssetCodes(None)
-IData.SetAssetCodes(['AFKS', 'AFLT', 'MOEX', 'SBER', 'VTBR', 'SU25084RMFS3', 'RU000A102RN7'], False)
-IData.SetBoards(['TQBR', 'TQOB', 'TQCB'])
-# IData.SetBoards(None)
-# data = IData.GetInterfaxData('MOEX', 'Securities', False)
+# getting info for specified asset codes
+IData.set_asset_codes(['AFKS', 'AFLT', 'MOEX', 'SBER', 'VTBR', 'SU25084RMFS3', 'RU000A102RN7'])
+data = IData.get_interfax_data('MOEX', 'Securities', False)
+
+# getting info for all securities traded on specified boards
+IData.set_asset_codes(None)
+IData.set_boards(['TQBR', 'TQOB', 'TQCB'])
+IData.set_boards(['TQCB'])
+# IData.set_boards(None)
+data = IData.get_interfax_data('MOEX', 'Securities', False)
 
 # POST futures
-IData.SetUnderlying('BRO')
-IData.SetAssetCodes(['BRX2BRZ2', 'BRV2BRX2', 'BRZ2BRF3'])
-# data = IData.GetInterfaxData('MOEX', 'Futures')
-
-IData.SetBoards(['RFUD'])
-IData.SetBoards(['TQBR'])
-IData.SetAssetCodes(None)
+IData.set_underlying('BRO')
+IData.set_asset_codes(['BRX2BRZ2', 'BRV2BRX2', 'BRZ2BRF3'])
+data = IData.get_interfax_data('MOEX', 'Futures')
 
 # ----------------------------------
 # ------- Archive Controller -------
 # ----------------------------------
 
+IData.set_dates('2023-01-01', '2023-07-31')
+IData.set_asset_codes(['AFKS'])
 data = IData.get_interfax_data('Archive', 'History')
+
+IData.set_currencies('USD', 'RUB')
+data = IData.get_interfax_data('Archive', 'CurrencyRateHistory')
+
+#data = IData.get_interfax_data('Info', 'Calendar')
+
+# -------------------------------
+# ------- BOND CONTROLLER -------
+# -------------------------------
+
+IData.set_boards(['TQOB', 'TQCB'])
+IData.set_boards(None)
+IData.set_asset_codes(['SU25084RMFS3', 'RU000A102RN7'])
+data = IData.get_interfax_data('Bond', 'Coupons', True)
+
+IData.save_data_to_db()
+
+IData.__parse_coupons_response()
+
+
+data = IData.get_interfax_data('Bond', 'AuctionData')
 
 # ----------------------------------
 # ------- Emitent Controller -------
 # ----------------------------------
 
-# Emitent list
-dbConn = pyodbc.connect(
-    "Driver={SQL Server Native Client 11.0};Server=LAPTOP-QBI0SKOK\\LOCALDB;Database=Analysis;Trusted_Connection=yes;")
 
-insertTemplate = "insert into dbo.IFX_Companies(FinInstId, ShortName, FullName, Sector, INN) " \
-    "values(%d, '%s', '%s', '%s', '%s')"
+roughData = IData.get_interfax_data('Info', 'Calendar', False)
 
-pageIndex = 1
-pageSize = 100
-IData.set_paging(pageIndex, pageSize)
-IData.update_body_paging()
-emitents = IData.get_interfax_data('Emitent', 'Companies')
-count = emitents[0]['counter']
-
-while True:
-    for emt in emitents:
-        cursor = dbConn.cursor()
-        if emt['shortname_rus'] is not None:
-            emt['shortname_rus'] = emt['shortname_rus'].replace("'", "''")
-        if emt['fullname_rus'] is not None:
-            emt['fullname_rus'] = emt['fullname_rus'].replace("'", "''")
-        # cursor.execute(
-            # insertTemplate % (emt['fininstid'], emt['shortname_rus'], emt['fullname_rus'], emt['sector'], emt['inn']))
-        # cursor.commit()
-    if pageIndex * pageSize >= count:
-        break
-    pageIndex += 1
-    IData.set_paging(pageIndex, pageSize)
-    IData.update_body_paging()
-    emitents = IData.get_interfax_data('Emitent', 'Companies')
-
-dbConn.close()
-
-# Find emitent
-IData.set_searching_pattern(['Полюс'])
-company = IData.get_interfax_data('Emitent', 'Find')
-
-# Multipliers
-dbConn = pyodbc.connect(
-    "Driver={SQL Server Native Client 11.0};Server=LAPTOP-QBI0SKOK\\LOCALDB;Database=Analysis;Trusted_Connection=yes;")
-mplDate = '2023-02-15'
-IData.set_dates(mplDate, mplDate)
-
-listTemplate =\
-    "select distinct ifx.FinInstId from dbo.DCT_Companies cmp join dbo.IFX_Companies ifx on cmp.INN = ifx.INN " \
-    "join dbo.DCT_Assets ast on ast.CompanyId = cmp.Id where ast.Id in (select SecurityId from dbo.IND_Structures) " \
-    "and ifx.Exclude = 0"
-
-cursor = dbConn.cursor()
-cursor.execute(listTemplate)
-idxIds = cursor.fetchall()
-
-insTemplate =\
-    "insert into dbo.IFX_Multipliers (" \
-    "FinInstId, [Date], CurrentRatio, TotalDebt, Earning, EBITDA, EnterpriseValue, EPS, EV_To_EBITDA, " \
-    "Equity, FFO, FCFF, FCFE, ROE) " \
-    "values('%d', '%s', %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-for finInstId in idxIds:
-    IData.set_fin_inst_ids(finInstId[0])
-    mpl = IData.get_interfax_data('Emitent', 'Multipliers')
-    mpl = mpl[0]
-    cursor = dbConn.cursor()
-    cursor.execute(insTemplate % (
-        finInstId[0], mplDate, mpl['currentRatio'] or 'NULL', mpl['debt'] or 'NULL', mpl['earnings'] or 'NULL',
-        mpl['ebitda'] or 'NULL', mpl['enterpriseValue'] or 'NULL', mpl['eps'] or 'NULL', mpl['evEbitda'] or 'NULL',
-        mpl['equity'] or 'NULL', mpl['ffo'] or 'NULL', mpl['fcff'] or 'NULL', mpl['fcfe'] or 'NULL',
-        mpl['roe'] or 'NULL'))
-    cursor.commit()
-print('End')
-
-dbConn.close()
-IData.set_fin_inst_ids([88793])
-IData.set_dates('2022-12-30', '2023-02-28')
-mpls = IData.get_interfax_data('Emitent', 'Multipliers')
+data = IData.get_interfax_data('Info', 'Calendar', True)
--- Data_Loading/ifxdata.py
+++ Data_Loading/ifxdata.py
@@ -4,10 +4,11 @@ import pyodbc
 import requests
 from datetime import date, timedelta
 from collections.abc import Iterable
+import math
 
-
+# Class for getting data from InterFax Web API
 class InterfaxData:
-    
+
     # interfax base url
     baseurl = 'https://dh2.efir-net.ru/v2'
     # interfax login & password
@@ -27,18 +28,26 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
 
         # initialize controller set        
         self.__init_controllers()
-        # initialize action methods for each controller
+        # initialize realized action methods for each controller
         self.__init_archive_actions()
         self.__init_bond_actions()
         self.__init_emitent_actions()
         self.__init_moex_actions()
+        self.__init_info_actions()
 
         self.proxies = self.body = None
         self.toDate = date.today() - timedelta(days=1)
         self.fromDate = self.toDate - timedelta(days=7)
+        
+        # link for saving data to DB
+        # used in called function save_data_to_db()
+        self.__saveDataToDB = None
 
         self.ifxCodes = {}
         self.ifxIds = {}
+        
+        self.baseCurr = 'USD'
+        self.quotedCurr = 'RUB'
 
         self.underlying = None
         self.boards = None
@@ -47,16 +56,22 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
 
         self.cmpnSearchPattern = None
 
-        self.roughData = None
+        self.roughData = self.parsedData = self.savedData = None
 
         self.pageNum = self.pageSize = None
 
     # Init controller set
     def __init_controllers(self):
+        # set of functions getting InterFax data
         self.controllers = {'Archive': {}, 'Bond': {}, 'CorporateAction': {},
                             'Emitent': {}, 'Indicator': {}, 'Info': {}, 'Rating': {}, 'MOEX': {}}
+        # set of parsers
         self.parsers = {'Archive': {}, 'Bond': {}, 'CorporateAction': {},
                         'Emitent': {}, 'Indicator': {}, 'Info': {}, 'Rating': {}, 'MOEX': {}}
+    
+        # set of saving functions
+        self.db_manager = {'Archive': {}, 'Bond': {}, 'CorporateAction': {},
+                           'Emitent': {}, 'Indicator': {}, 'Info': {}, 'Rating': {}, 'MOEX': {}}
 
     # Return controller names
     def controllers(self):
@@ -126,6 +141,8 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
         self.controllers[controller][action]()
         # get requested data
         self.roughData = self.__do_post_request()
+        # init the savink link
+        #self.__saveDataToDB = self.db_manager[controller][action]
         # return data
         return self.parsers[controller][action]() if parse else self.roughData
 
@@ -137,6 +154,14 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
             self.headers = {'authorization': 'Bearer ' + self.token, 'Content-Type': 'application/json'}
         response = requests.post(self.url, json=self.body, headers=self.headers, proxies=self.proxies, verify=False)
         return response.json() if response.status_code == 200 else None
+
+    # Method called for saving data to DB
+    def save_data_to_db(self):
+        if self.__saveDataToDB is None:
+            print("No data to save")
+        else:
+            self.__saveDataToDB()
+            print("Data have been saved")
 
     # ---------------------------------------------------------------
     # ------- Setting params used when setting actions bodies -------
@@ -183,6 +208,7 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
 
         self.__update_ifx_codes()
 
+    # Updating IFX codes for passed MOEX codes
     def __update_ifx_codes(self):
         if len(self.ifxCodes) <= 0:
             return
@@ -191,16 +217,10 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
             self.ifxCodes[item['secid']] = item['fintoolid']
             self.ifxIds[item['fintoolid']] = {'secid': item['secid'], 'ISIN': item['isin'], 'id_iss': item['id_iss']}
 
-    def __update_fintoolids_for_asset_codes_from_db(self):
-        if self.assetCodes is None:
-            self.assetFintoolIds = self.fintoolIds = None
-            return
-        cursor = self.sql_conn.cursor()
-        cds = str(self.assetCodes).replace('[', '(').replace(']', ')')
-        cursor.execute("select FintoolId from dbo.DCT_Assets where MOEX_Code in %s" % cds)
-        data = cursor.fetchall()
-        self.assetFintoolIds = list(set([ids[0] for ids in data]))
-        cursor.close()
+    # Set base and quoted currencies for getting exchange rates
+    def set_currencies(self, base_curr, quoted_curr):
+        self.baseCurr = base_curr
+        self.quotedCurr = quoted_curr
 
     def set_searching_pattern(self, pattern):
         self.cmpnSearchPattern = pattern
@@ -211,56 +231,141 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
     # ------------------------------------------
     # ------- Archive controller actions -------
     # ------------------------------------------
+    
+    # Init all realized methods for Archive controller
     def __init_archive_actions(self):
         self.controllers['Archive']['History'] = self.__set_archive_history_body
+        self.controllers['Archive']['CurrencyRateHistory'] = self.__set_archive_currencyratehistory_body
 
+    #  set request body for Archive/History method
     def __set_archive_history_body(self):
         key = list(self.ifxIds.keys())[0]
         self.body = \
             {'dateFrom': self.fromDate, 'dateTo': self.toDate, 'issId': self.ifxIds[key]['id_iss'], 'step': 1440}
+            
+    # Set request body for Archive/CurrencyRateHistory method
+    def __set_archive_currencyratehistory_body(self):
+        self.body = \
+            {'dateFrom': self.fromDate, 'dateTo': self.toDate, 'baseCurrency': self.baseCurr, 'quotedCurrency': self.quotedCurr, 'step': 1440}
 
     # ---------------------------------------
     # ------- Bond controller actions -------
     # ---------------------------------------
+    
+    # Init all realized methods for Bond cotroller
     def __init_bond_actions(self):
         # action methods
         self.controllers['Bond']['AuctionData'] = self.__set_bond_auction_body
         self.controllers['Bond']['Coupons'] = self.__set_bond_coupons_body
+        self.controllers['Bond']['Convertation'] = self.__set_bond_convertation_body
         # parsers
-        self.parsers['Bond']['Coupons'] = self.__parse_coupons_response
+        self.parsers['Bond']['Coupons'] = self.__parse_bond_coupons_response
+        # savers
+        self.db_manager['Bond']['Coupons'] = self.__save_bond_coupons_from_rough_data
 
+    # Init request body for Bond/Auction method
     def __set_bond_auction_body(self):
         self.body = {'filter': 'id_fintool = %s' % list(self.ifxIds.keys())[0]}
 
+    # Init request body for Bond/Coupon method
     def __set_bond_coupons_body(self):
         ids = str(list(self.ifxIds.keys())).replace('[', '(').replace(']', ')')
         self.body = {'filter': 'id_fintool in %s' % ids}
+        pass
 
-    def __parse_coupons_response(self):
+    # Init request body for Bond/Convertation method
+    def __set_bond_convertation_body(self):
+        pass
+
+    # Init request body for Bond/Coupons method
+    def __parse_bond_coupons_response(self):
         if self.roughData is None:
             return None
-        parsed_data = pd.DataFrame()
-        for datum in self.roughData:
-            # parsed_data.append((datum['id_fintool'], datum['coupon_rate']))
-            parsed_data.append((datum['id_fintool'], datum['pay_date']))
+        dct = {'FinToolId': {}, 'CouponPeriod': {}, 'PeriodFrom': {}, 'PeriodTo': {}, 'PayPerBond': {}, 'CouponRate': {}}
+        self.parsedData = pd.DataFrame(dct)
 
-        return parsed_data
+        for datum in self.roughData:
+            row = {'FinToolId': int(datum['id_fintool']), 'CouponPeriod': datum['id_coupon'],
+                   'PeriodFrom': datum['begin_period'], 'PeriodTo': datum['end_period'], 'PayPerBond': datum['pay_per_bond'], 'CouponRate': datum['coupon_rate']}
+            # parsed_data.append((datum['id_fintool'], datum['pay_date'], datum['period_from'], datum['period_to'], datum['pay_per_bond']))
+            self.parsedData = self.parsedData.append(row, ignore_index = True)
+
+        # init the savink link
+        # self.__saveDataToDB = self.db_manager['Bond']['Coupons']
+
+        return self.parsedData
+
+    # Saving data to DB from Bond/Coupons method from rough data
+    # Used for setting self.__saveDataToDB link in get_interfax_data method
+    def __save_bond_coupons_from_rough_data(self):
+        # connect to db
+        self.sql_connect()
+        
+        # list of instruments Id
+        ids = str(list(self.ifxIds.keys())).replace('[', '(').replace(']', ')')        
+
+        # delete existed data
+        cursor = self.sql_conn.cursor()
+        cursor.execute("delete from dbo.ACF_Coupons where FintoolId in %s" % ids)
+        cursor.commit()
+        
+        # disconnect from db
+        self.sql_disconnect()
+   
+    # Saving data to DB from Bond/Coupons method from parsed data
+    def __save_bond_coupons_from_parsed_data(self):
+        
+        self.savedData = self.parsedData
+        
+        # connect to db
+        self.sql_connect()
+
+        # list of instruments Id
+        ids = str(list(self.ifxIds.keys())).replace('[', '(').replace(']', ')')
+
+        # delete existed data
+        cursor = self.sql_conn.cursor()
+        cursor.execute("delete from dbo.ACF_Coupons where FintoolId in %s" % ids)
+        cursor.commit()
+
+        # from "insert" script template
+        insSQL = "insert into dbo.ACF_Coupons (FintoolId, CouponPeriod, StartDate, EndDate, [Value]) values(%s, %s, '%s', '%s', %f)"
+
+        # insert data from parsed data set
+        for index, row in self.savedData.iterrows():
+            # skip empty data
+            if math.isnan(row['PayPerBond']):
+                continue
+            # from end execute sql command
+            cursor.execute(insSQL % (row['FinToolId'], row['CouponPeriod'], row['PeriodFrom'][:10], row['PeriodTo'][:10], row['PayPerBond']))
+            cursor.commit()
+
+        # disconnect from db
+        self.sql_disconnect()
+        
+        # reset the saving link
+        self.__saveDataToDB = None
 
     # ------------------------------------------
     # ------- Emitent controller actions -------
     # ------------------------------------------
+    
+    # Init all realized methods for Emitent controller
     def __init_emitent_actions(self):
         self.controllers['Emitent']['Companies'] = self.__set_emitemt_companies_body
         self.controllers['Emitent']['Find'] = self.__set_emitent_find_body
         self.controllers['Emitent']['Multipliers'] = self.__set_emitent_multipliers_body
 
+    # Set request body for Emitent/Companies method
     def __set_emitemt_companies_body(self):
         self.body = {}
         self.update_body_paging()
 
+    # Set request body for Emitent/Find method
     def __set_emitent_find_body(self):
         self.body = {'codes': self.cmpnSearchPattern}
 
+    # Set request body for Emitent/Multipliers method
     def __set_emitent_multipliers_body(self):
         self.body = {"fininstIds": [self.finInstIds]}
         if self.fromDate is not None:
@@ -271,13 +376,16 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
     # ---------------------------------------
     # ------- MOEX controller actions -------
     # ---------------------------------------
+    
+    # Init all realized methods for MOEX controller
     def __init_moex_actions(self):
         self.controllers['MOEX']['Securities'] = self.__set_moex_securities_body
         self.controllers['MOEX']['Futures'] = self.__set_moex_futures_body
 
+    # Init request body for MOEX/Securities method
     def __set_moex_securities_body(self):
         self.body = {'pageNum': 1, 'pageSize': 100000}
-        if self.ifxCodes is not None:
+        if self.ifxCodes is not None and len(self.ifxCodes) > 0:
             codes = list(self.ifxCodes.keys())
             self.body['filter'] = "secid in %s" % (str(codes).replace('[', '(').replace(']', ')'))
         if self.boards is not None:
@@ -287,8 +395,38 @@ Database=AnalyticDev;Trusted_Connection=yes;"""
             else:
                 self.body['filter'] = board_filter
 
+    # Set request body for MOEX/Futures method
     def __set_moex_futures_body(self):
         self.body = {'filter': 'EXPIRATION_DATE > #%s#' % self.toDate}
         if self.underlying is not None:
             self.body['filter'] += " and BA_SECID = '%s'" % self.underlying
         # self.body = {'filter' : "BA_SECID = 'VTBR'"}
+    
+    # ---------------------------------------
+    # ------- Info controller actions -------
+    # ---------------------------------------
+
+    # Init all realized methods for Info controller
+    def __init_info_actions(self):
+        self.controllers['Info']['Calendar'] = self.__set_info_calendar_body
+        
+        #11.вызов парсеров
+        self.parsers['Info']['Calendar'] = self.__parse_info_calendar_response
+        
+    def __set_info_calendar_body(self):
+        today = date.today()
+        self.body = {"eventTypes": ["DIV"],
+          "fields": ["isiNcode","nickname","pay1Security", "fixDate","faceFTName","recomendFixDate","recomendPay1Security","rateDate"],
+          "filter":f"(recomendFixDate >= #{today}#) and faceFTName='RUB'"}
+    
+    def __parse_info_calendar_response(self):
+        if self.roughData is None:
+            return None
+        dct = {'Isin': {}, 'name': {}, 'recomendFixDate' :{}}
+        self.parsedData = pd.DataFrame(dct)
+        
+        for datum in self.roughData['timeTableFields']:
+            row = {'Isin': datum['isiNcode'], 'name': datum['nickname'], 'recomendFixDate': datum['recomendFixDate']}
+            self.parsedData = self.parsedData.append(row, ignore_index = True)
+        
+        return self.parsedData
